de9358d46783991f54cf6351d3b16b9e8194b12b
Hexuan Li <lihexuan1@gmail.com>
2022-05-20 20:11:13 -0700

    add reaction schema to model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,38 @@
 const { Schema, model, Types } = require('mongoose');
 // import moment module to format the timestamp 
 const moment = require('moment')
+
+//reaction schema
+const reactionSchema = new Schema (
+    {
+       reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+       },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+
 // thought schema
 const thoughtSchema = new Schema (
     {
@@ -24,10 +56,12 @@ const thoughtSchema = new Schema (
     {
         toJSON: {
             virtuals: true,
+            getters: true,
         },
         id: false,
     }
 )
+
 
 // get total count of friends
 thoughtSchema.virtual('reactionCount')
