2d95ec958f1b903e78290d7e14a1767b94efd767
Hexuan Li <lihexuan1@gmail.com>
2022-05-20 21:19:35 -0700

    add outline for controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,18 @@
+const { model } = require('mongoose');
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all thoughts
+
+    // get single thought
+
+    //create a thought
+
+    //update a thought
+
+    //delete a thought
+
+    //create reaction
+
+    //delete reaction
+};
---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    //Get al users
+
+    //Get single user
+
+    //create a user
+
+    //update a user
+
+    //delete a user
+    //BONUS: Remove a user's associated thoughts when deleted.
+
+    //add a friend
+
+    //delete a friend
+};
