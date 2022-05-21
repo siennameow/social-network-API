a37e2437efb9570fb9164387222b50937d6015c1
Hexuan Li <lihexuan1@gmail.com>
2022-05-21 09:42:05 -0700

    add function to create/update user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,9 +19,28 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
     //create a user
-
+    createUser(req,res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
     //update a user
-
+    updateUser(req,res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+        !user
+            ? res.status(404).json({ message: 'No User find with this ID!'})
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
     //delete a user
     //BONUS: Remove a user's associated thoughts when deleted.
 
